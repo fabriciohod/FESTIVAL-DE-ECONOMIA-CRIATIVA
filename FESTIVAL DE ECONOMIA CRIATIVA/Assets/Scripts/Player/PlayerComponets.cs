a342eb89b111841f9464cb4b25f43de43ee1c924
fabriciohod <fabriciohod@hotmail.com>
2020-03-01 18:49:32 -0300

    _

--- FESTIVAL DE ECONOMIA CRIATIVA/Assets/Scripts/Player/PlayerComponets.cs
+++ FESTIVAL DE ECONOMIA CRIATIVA/Assets/Scripts/Player/PlayerComponets.cs
@@ -1,23 +1,22 @@
 ﻿#region Using
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 #endregion
-    public class PlayerComponets : MonoBehaviour
+    namespace Assets.Scripts.Player
     {
-        Rigidbody2D _rb;
-        Transform _transform;
-        SpriteRenderer _render;
-
-        public Rigidbody2D Rb { get => _rb; private set => _rb = value; }
-        public Transform Transform { get => _transform; private set => _transform = value; }
-        public SpriteRenderer Render { get => _render; private set => _render = value; }
-
-        void Awake ()
+        public class PlayerComponets : MonoBehaviour
         {
-            Rb = GetComponent<Rigidbody2D>();
-            Transform= GetComponent<Transform>();
-            Render = GetComponent<SpriteRenderer>();
-        }
+            public Rigidbody2D Rb { get; private set; }
 
+            public Transform Transform { get; private set; }
+
+            public SpriteRenderer Render { get; private set; }
+
+            private void Awake ()
+            {
+                Rb = GetComponent<Rigidbody2D>();
+                Transform= GetComponent<Transform>();
+                Render = GetComponent<SpriteRenderer>();
+            }
+
+        }
     }
--- FESTIVAL DE ECONOMIA CRIATIVA/Assets/Scripts/Player/PlayerInputHandler.cs
+++ FESTIVAL DE ECONOMIA CRIATIVA/Assets/Scripts/Player/PlayerInputHandler.cs
@@ -1,49 +1,48 @@
 ﻿#region Using
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+
 #endregion
-namespace Player.InputHandler
+namespace Assets.Scripts.Player
 {
     [RequireComponent (typeof (Rigidbody2D))]
     public class PlayerInputHandler : MonoBehaviour
     {
         #region Private Field´s
-        private bool EstaNoChao = true;
+        private bool _estaNoChao = true;
         #endregion
 
         #region Serialize Field
-        [SerializeField][Range (0.15f, 0.60f)] float speed = 0.15f;
-        [SerializeField][Range (300f, 700f)] float jumpForce = 300f;
-        [SerializeField] private PlayerComponets PlayerComponets;
+        [SerializeField][Range (0.15f, 0.60f)] private float _speed = 0.15f;
+        [SerializeField][Range (300f, 700f)] private float _jumpForce = 300f;
+        [SerializeField] private PlayerComponets _playerComponets;
         #endregion
 
         private void Update ()
         {
-            MovimentacaoHorizontal(Input.GetAxisRaw("Horizontal"), PlayerComponets.Transform);
-            Jump (Input.GetKeyDown(KeyCode.Space), PlayerComponets.Rb);            
+            MovimentacaoHorizontal(Input.GetAxisRaw("Horizontal"), _playerComponets.Transform);
+            Jump (Input.GetKeyDown(KeyCode.Space), _playerComponets.Rb);            
         }
 
         private void MovimentacaoHorizontal (float axis, Transform transform)
         {
-            if (axis > 0) { transform.Translate (Vector2.right * speed); }
+            if (axis > 0) { transform.Translate (Vector2.right * _speed); }
 
-            if (axis < 0) { transform.Translate (Vector2.left * speed); }
+            if (axis < 0) { transform.Translate (Vector2.left * _speed); }
         }
         private void Jump (bool bind, Rigidbody2D rb)
         {
-            if (bind is true && EstaNoChao is true) { rb.AddForce (Vector2.up * jumpForce); }
+            if (bind is true && _estaNoChao is true) { rb.AddForce (Vector2.up * _jumpForce); }
         }
 
         private void OnCollisionEnter2D (Collision2D collision)
         {
-            if (collision.collider.tag is "Chao") { _EstaNoChao = true; }
+            if (collision.collider.tag is "Chao") { _estaNoChao = true; }
         }
 
         private void OnCollisionExit2D (Collision2D collision)
         {
-            if (collision.collider.tag is "Chao") { _EstaNoChao = false; }
+            if (collision.collider.tag is "Chao") { _estaNoChao = false; }
         }
     }
 }
