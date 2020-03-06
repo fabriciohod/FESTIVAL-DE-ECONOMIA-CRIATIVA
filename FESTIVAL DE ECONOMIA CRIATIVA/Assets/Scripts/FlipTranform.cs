2cfaca9ce6fdbc5513c74e86fddca079aa997009
fabriciohod <fabriciohod@hotmail.com>
2020-03-05 22:18:52 -0300

    _

--- FESTIVAL DE ECONOMIA CRIATIVA/Assets/Scripts/FlipTranform.cs
+++ FESTIVAL DE ECONOMIA CRIATIVA/Assets/Scripts/FlipTranform.cs
@@ -3,22 +3,26 @@
 public class FlipTranform : MonoBehaviour
 {
     private Transform _transform;
+    private Vector3 _position;
+
 
     private void Awake()
     {
         _transform = GetComponent<Transform>();
+        _position = _transform.position;
     }
+
     [ContextMenu("teste Flip")]
     public void Flip()
     {
-        _transform.SetPositionAndRotation(new Vector3(_transform.position.x * -1, _transform.position.y, 0),
+        _transform.SetPositionAndRotation(new Vector3(_position.x * -1, _position.y, 0),
             Quaternion.Euler(0f, 180f, 0));
-        
     }
+
     [ContextMenu("teste InverseFlip")]
     public void InverseFlip()
     {
-        _transform.SetPositionAndRotation(new Vector3(_transform.position.x * 1, _transform.position.y, 0),
+        _transform.SetPositionAndRotation(new Vector3(_position.x * 1, _position.y, 0),
             Quaternion.Euler(0f, 0f, 0));
     }
 }
